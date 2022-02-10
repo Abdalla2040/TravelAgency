381c9a8b64cfbb512ff9f6989f8e3ca9a045b768
Abdullahi Mohamed <almohamed@ad.scmn.net>
2022-02-10 10:19:06 -0600

    changes 2

--- Final__Project/Form2.cs
+++ Final__Project/Form2.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
+using System.Data.SqlClient;
 
 namespace Final__Project
 {
@@ -19,7 +20,8 @@ namespace Final__Project
         }
         string path = @"TextFile2.txt";
         //string itemsToCollect = Form1.selecteditem + "\n";
-        string[] itemsList;
+        List<string> list = new List<string>();
+        StreamReader reader;
             
            
             
@@ -27,15 +29,23 @@ namespace Final__Project
         //Form8 form8 = new Form8();
         public List<string> coll = new List<string>();
         //DataSet1TableAdapters.CalculationsTableAdapter cal = new DataSet1TableAdapters.CalculationsTableAdapter();
+        
+
+
         private void btnAdd_Click(object sender, EventArgs e)
         {
-            
-            listBox1.Items.Add(Form1.selecteditem);
-            coll.Add(Form1.selecteditem);
-            //Form4 form4 = new Form4();
-            //form4.ShowDialog();
-            //this.Close();
-         
+            // reader = File.OpenText(@"TextFile2.txt");
+            //int length = list.Count-1;
+            //foreach(string item in list)
+            //    listBox1.Items.Add(item);
+            //listBox1.Items.Add(list[length]);
+            foreach (string item in list)
+                listBox1.Items.Add(item);
+
+
+
+
+
 
 
         }
@@ -46,10 +56,31 @@ namespace Final__Project
             //string path = @"TextFile2.txt";
             //File.WriteAllText(path, itemsToCollect);
             // //string[] file2 = File.WriteAllLines(path, itemsList)
-            File.AppendAllText(path, Form1.selecteditem + "\n");
-            string[] file = File.ReadAllLines(path);
-            foreach(var n in file)
-                listBox1.Items.Add(n);
+            //File.AppendAllText(path, Form1.selecteditem + "\n");
+            //string[] file = File.ReadAllLines(path);
+            //foreach(var n in file)
+            //    listBox1.Items.Add(n);
+            reader = File.OpenText(@"TextFile2.txt");
+            while (!reader.EndOfStream)
+            {
+                
+                list.Add(reader.ReadLine());
+                //
+                //for(int i = 0; i < length; i++)
+                //{
+                //    listBox1.Items.Add(list[i]);
+                //}
+                
+                
+            }
+            reader.Close();
+            //int length = list.Count - 1;
+            
+            //if (list.Count < length)
+            //    
+         
+                
+            
 
         }
 
